e54d404155237aa71ffaf4eea8b0e7869926660e
Christopher Atwell <catwell@mys3tech.com>
2024-11-15 12:25:02 -0500

    Update __init__.py

--- TimerTriggerFunction/__init__.py
+++ TimerTriggerFunction/__init__.py
@@ -5,9 +5,7 @@ def main(mytimer):
     logging.info("Timer trigger function started.")
     try:
         response = requests.get("https://catfact.ninja/fact")
-        response.raise_for_status()
-        fact = response.json().get("fact", "No fact found")
-        logging.info(f"Cat Fact: {fact}")
+        logging.info(f"Cat Fact: {response}")
     except Exception as e:
         logging.error(f"Error occurred: {e}")
         raise  # Re-raise the error to propagate it to Azure logs
